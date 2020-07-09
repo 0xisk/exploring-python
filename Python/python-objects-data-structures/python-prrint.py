39e69e6fb2bb51f62502274ae624e1bc512d4786
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 21:55:47 +0000

    Practicing print-formating, python.

---
+++ Python/python-objects-data-structures/python-prrint.py
@@ -0,0 +1,13 @@
+print('This is a {} string'.format('Inserted'))
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+print('The {0} {0} {0}'.format('fox', 'brown', 'quick'))
+print('The {f} {f} {f}'.format(f='fox', b='brown', q='quick'))
+
+result = 100/777
+print('The result was {r}'.format(r=result))
+print('The result was {r:1.3f}'.format(r=result))
+
+# f-string and this new to python 3.6
+name = 'iskander'
+print(f'Hello, his name is {name}')
