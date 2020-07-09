1244d04401364a3170bf71528e9f6c643374e32c
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 10:32:12 +0000

    Practicing Python Strings indexing and slicing, python

--- Python/python-objects-data-structures/python-string.py
+++ Python/python-objects-data-structures/python-string.py
@@ -4,3 +4,15 @@
 "I'm going"
 print("Hello")
 len('Iskander Andrews')
+
+myString = "Hello World"
+myString[0]
+myString[-1]
+myString[-7] 
+
+myString[2:]
+myString[:3]
+myString[3:6]
+myString[::4]
+myString[::-1]
+myString[0:4:2]
