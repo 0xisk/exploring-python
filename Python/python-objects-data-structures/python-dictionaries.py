6176d69370177bc6cdd46f2e85f0e588e19a9749
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 22:44:49 +0000

    Practicing dictionaries, python.

---
+++ Python/python-objects-data-structures/python-dictionaries.py
@@ -0,0 +1,13 @@
+my_dict = { 'Key1': 'value', 'key2': 'value2' }
+my_dict['key2']
+
+prices_lookup = {'apples': 2.99, 'oranges': 1.99}
+prices_lookup['apples']
+
+d = {'k1': 123, 'k2': [0,1,2], 'k3': {'insideKey':100}}
+d['k2']
+d['k3']['insideKey']
+d['k4'] = 1000
+d
+d.keys()
+d.values()
