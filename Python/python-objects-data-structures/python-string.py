efad804f3fd7c597d686b45ba507859e2a3e0af1
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 10:44:47 +0000

    Python strings, python/

--- Python/python-objects-data-structures/python-string.py
+++ Python/python-objects-data-structures/python-string.py
@@ -14,5 +14,12 @@ myString[2:]
 myString[:3]
 myString[3:6]
 myString[::4]
-myString[::-1]
+myString[::-1] # Inverse Array
 myString[0:4:2]
+
+name = "Sam"
+# name[0] = "H"
+last_letters = name[1:]
+last_letters
+'P' + last_letters
+last_letters * 1000000000
